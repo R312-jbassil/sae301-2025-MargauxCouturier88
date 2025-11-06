a0b9450abb9888f10cde3cb8decdff382f3ffef8
MargauxCouturier88 <margaux.couturier02@edu.univ-fcomte.fr>
2025-11-06 08:58:55 +0100

    Ajout de l'adaptateur Node pour la configuration Astro et mise à jour des dépendances dans package.json et package-lock.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
@@ -8,5 +9,9 @@ export default defineConfig({
   output: 'server',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  // configure Node adapter for server output
+  adapter: node(),
 });
+
+
