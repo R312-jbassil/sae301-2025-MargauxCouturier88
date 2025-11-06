1c6ca6f4925b14b8484dcf6894966cb61f2725a8
MargauxCouturier88 <margaux.couturier02@edu.univ-fcomte.fr>
2025-11-06 09:03:42 +0100

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,12 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
   vite: {
     plugins: [tailwindcss()]
   },
-  // configure Node adapter for server output
-  adapter: node(),
+  adapter: node({
+    mode: 'standalone'
+  })
 });
-
-
